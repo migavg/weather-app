5f8d181ff2a9b527dc85e4dad206a96449182a47
mvill <119754846+migavg@users.noreply.github.com>
2024-05-01 17:51:44 -0700

    added bare weather api set to phoenix on script file

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,21 @@ const weatherBLOCK = document.getElementById("weatherBlock")
 
 
 function pullWeather()  {
+const weatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=Phoenix&appid=1585e0bbf2dbcb702a5708a67bf3b74d&units=imperial";
 
+    fetch(weatherAPI)
+    .then(function(response){
+        return response.json();
+    })
+
+    .then(function (data)
+    {
+
+    
     console.log("test")
+    console.log(data)
+}
+)
 };
 
 
